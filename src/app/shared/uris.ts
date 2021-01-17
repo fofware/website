8bd6c7099b32670cf61fa9e097af5973a67460ce
fofware <fofware@gmail.com>
2021-01-17 10:38:41 -0300

    Modifica URIS por cambio en servidor web

--- src/app/shared/uris.ts
+++ src/app/shared/uris.ts
@@ -1,2 +1,4 @@
-export const API_URI = `${location.protocol}//firulais.net.ar:${(location.protocol == 'https:' ? 3443 : 3000)}/api`
-export const AUTH_URI = `${location.protocol}//firulais.net.ar:${(location.protocol == 'https:' ? 3443 : 3000)}`
+// export const API_URI = `${location.protocol}//firulais.net.ar:${(location.protocol == 'https:' ? 3443 : 3000)}/api`
+// export const AUTH_URI = `${location.protocol}//firulais.net.ar:${(location.protocol == 'https:' ? 3443 : 3000)}`
+export const API_URI = `${location.protocol}//firulais.net.ar/api`;
+export const AUTH_URI = `${location.protocol}//firulais.net.ar`;
